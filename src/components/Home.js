1e9af8f316bf5b8e81e1938eaa42d3ed4374ffff
zachary oliver <zspoliver@gmail.com>
2024-04-23 17:59:30 -0400

    adds components

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={{color: 'firebrick'}}>{name} is a Web Developer for {city}</h1>
+    </div>
+  )
 }
 
 export default Home;
